c70f109a3fd1f6e3a0283030ae8ce7d4796ad1b9
David <davidcha@email.unc.edu>
2023-12-12 22:33:50 -0500

    starting reading tracker app

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const host = "localhost";
+const port = 8000;
+
+// define endpoints
+// add book
+// remove book
+// display books
+
+const server = http.createServer();
